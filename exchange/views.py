8c9043b6a48bcb6e22a1c48fb031d78336e87b94
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2026-03-10 09:07:29 +0100

    add view home page

--- exchange/views.py
+++ exchange/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import HelpOffer
 
-# Create your views here.
+
+class HomeView(ListView):
+    model = HelpOffer
+    template_name = 'exchange/home.html'
+    context_object_name = 'Offers'
+    
+    def get_queryset(self):
+        return HelpOffer.objects.select_related('help_request__requester', 'help_request__skill_needed').order_by('-created_at')
+    
+
