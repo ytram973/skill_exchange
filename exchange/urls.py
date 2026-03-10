5bf602479b122499a57c57c13ce421d70f146cf6
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2026-03-10 11:33:49 +0100

    auth login logout

--- exchange/urls.py
+++ exchange/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
+    path("skills/", views.SkillListView.as_view(), name="skill_list"),
+    path("my-skills/", views.MySkillsView.as_view(), name="my_skills"),
+    path("help-request/create/", views.HelpRequestCreateView.as_view(), name="help_request_create"),
+    path("help-request/list/", views.HelpRequestListView.as_view(), name="help_request_list"),
+    path("help-request/<int:pk>/offer/", views.MakeOfferView.as_view(), name="make_offer"),  
 ]
--- skillexchange/settings.py
+++ skillexchange/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = "dashboard"
+
+LOGIN_URL = "login"
