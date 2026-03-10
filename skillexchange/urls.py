b107c0f8446b5cd2fcb422b5dabd3c6330e58d91
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2026-03-10 11:36:40 +0100

    urls redirection route

--- skillexchange/urls.py
+++ skillexchange/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("exchange.urls")),
     path("login/", auth_views.LoginView.as_view(template_name="exchange/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
 ]
