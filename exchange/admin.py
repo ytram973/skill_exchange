a716d616fa43d89e196b494f1f42bd5a8431ba4f
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2026-03-10 09:00:56 +0100

    add admin file

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Skill, UserSkill, HelpRequest, HelpOffer
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name",)
+
+
+@admin.register(UserSkill)
+class UserSkillAdmin(admin.ModelAdmin):
+    list_display = ("user", "skill")
+    list_filter = ("skill",)
+
+
+@admin.register(HelpRequest)
+class HelpRequestAdmin(admin.ModelAdmin):
+    list_display = ("requester", "skill_needed", "date", "is_taken")
+    list_filter = ("is_taken", "skill_needed")
+    search_fields = ("activity_description",)
+
+
+@admin.register(HelpOffer)
+class HelpOfferAdmin(admin.ModelAdmin):
+    list_display = ("helper", "help_request", "created_at")
